6337cdee6a4be29bfd6316460733cca23f8c9f3a
kagawagao <kingsongao1221@gmail.com>
2020-09-29 19:06:49 +0800

    feat: update demo

--- src/App.vue
+++ src/App.vue
@@ -1,128 +1,83 @@
 <template>
-  <a-table :columns="columns" :data-source="data" bordered>
-    <template v-slot:name="{ text }">
-      <a>{{ text }}</a>
+  <a-button v-on:click="loadData">Load Data</a-button>
+  <a-table
+    :columns="columns"
+    :rowSelection="{ fixed: true }"
+    :data-source="data"
+    :scroll="{ x: 1300 }"
+  >
+    <template #action>
+      <a>action</a>
     </template>
   </a-table>
+  <div>耗时{{ time }}ms</div>
 </template>
 <script>
-// In the fifth row, other columns are merged into first column
-// by setting it's colSpan to be 0
-const renderContent = ({ text, index }) => {
-  const obj = {
-    children: text,
-    props: {},
-  };
-  if (index === 4) {
-    obj.props.colSpan = 0;
-  }
-  return obj;
-};
+const columns = [
+  {
+    title: "Full Name",
+    width: 100,
+    dataIndex: "name",
+    key: "name",
+    fixed: "left",
+  },
+  { title: "Age", width: 100, dataIndex: "age", key: "age", fixed: "left" },
+  { title: "Column 1", dataIndex: "address", key: "1" },
+  { title: "Column 2", dataIndex: "address", key: "2" },
+  { title: "Column 3", dataIndex: "address", key: "3" },
+  { title: "Column 4", dataIndex: "address", key: "4" },
+  { title: "Column 5", dataIndex: "address", key: "5" },
+  { title: "Column 6", dataIndex: "address", key: "6" },
+  { title: "Column 7", dataIndex: "address", key: "7" },
+  { title: "Column 8", dataIndex: "address", key: "8" },
+  {
+    title: "Action",
+    key: "operation",
+    fixed: "right",
+    width: 100,
+    slots: { customRender: "action" },
+  },
+];
 
 const data = [
   {
     key: "1",
     name: "John Brown",
     age: 32,
-    tel: "0571-22098909",
-    phone: 18889898989,
-    address: "New York No. 1 Lake Park",
+    address: "New York Park",
   },
   {
     key: "2",
     name: "Jim Green",
-    tel: "0571-22098333",
-    phone: 18889898888,
-    age: 42,
-    address: "London No. 1 Lake Park",
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    tel: "0575-22098909",
-    phone: 18900010002,
-    address: "Sidney No. 1 Lake Park",
-  },
-  {
-    key: "4",
-    name: "Jim Red",
-    age: 18,
-    tel: "0575-22098909",
-    phone: 18900010002,
-    address: "London No. 2 Lake Park",
-  },
-  {
-    key: "5",
-    name: "Jake White",
-    age: 18,
-    tel: "0575-22098909",
-    phone: 18900010002,
-    address: "Dublin No. 2 Lake Park",
+    age: 40,
+    address: "London Park",
   },
 ];
 
 export default {
   data() {
-    const columns = [
-      {
-        title: "Name",
-        dataIndex: "name",
-        customRender: ({ text, index }) => {
-          if (index < 4) {
-            return <a href="javascript:;">{text}</a>;
-          }
-          return {
-            children: <a href="javascript:;">{text}</a>,
-            props: {
-              colSpan: 5,
-            },
-          };
-        },
-      },
-      {
-        title: "Age",
-        dataIndex: "age",
-        customRender: renderContent,
-      },
-      {
-        title: "Home phone",
-        colSpan: 2,
-        dataIndex: "tel",
-        customRender: ({ text, index }) => {
-          const obj = {
-            children: text,
-            props: {},
-          };
-          if (index === 2) {
-            obj.props.rowSpan = 2;
-          }
-          // These two are merged into above cell
-          if (index === 3) {
-            obj.props.rowSpan = 0;
-          }
-          if (index === 4) {
-            obj.props.colSpan = 0;
-          }
-          return obj;
-        },
-      },
-      {
-        title: "Phone",
-        colSpan: 0,
-        dataIndex: "phone",
-        customRender: renderContent,
-      },
-      {
-        title: "Address",
-        dataIndex: "address",
-        customRender: renderContent,
-      },
-    ];
     return {
-      data,
+      data: [],
       columns,
+      time: 0,
     };
+  },
+  created() {
+    const start = Date.now();
+    this.$nextTick(() => {
+      const end = Date.now();
+      this.time = end - start;
+    });
+  },
+  methods: {
+    loadData() {
+      this.data = data;
+      const start = Date.now();
+      this.$nextTick(() => {
+        const end = Date.now();
+        this.time = end - start;
+      });
+    },
   },
 };
 </script>
